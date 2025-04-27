5feb07f773708216a186edff043d32d19b53046a
houtaib <houtyab@gmail.com>
2025-04-27 16:00:53 +0100

    for now just getting the message and print it in the serverterminal

--- main.rs
+++ main.rs
@@ -1,11 +1,10 @@
 use std::net::{TcpListener, TcpStream};
-use std::result;
 use std::io::Read;
 use std::fmt;
 use std::thread;
 use std::sync::mpsc::{Sender, Receiver, channel};
 
-type Result<T> = result::Result<T, ()>;
+type Result<T> = std::result::Result<T, ()>;
 
 struct Sensitive<T>(T);
 
@@ -13,11 +12,10 @@ const SAFE_MODE: bool = false;
 
 impl<T: fmt::Display> fmt::Display for Sensitive<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let Self(inner) = self;
         if SAFE_MODE {
-            writeln!(f, "[REDACTED]")
+            write!(f, "[REDACTED]")
         } else {
-            inner.fmt(f)
+            write!(f, "{}", self.0)
         }
     }
 }
@@ -28,25 +26,45 @@ enum Message {
     NewMessage(Vec<u8>),
 }
 
-fn server(_message: Receiver<Message>) -> Result<()> {
-       todo!("{:?}", ())
+fn server(_receiver: Receiver<Message>) -> Result<()> {
+    // Just stubbed out here, you can handle messages from clients
+    for message in _receiver {
+        match message {
+            Message::ClientConnected => println!("INFO: Client connected"),
+            Message::ClientDisconnected => println!("INFO: Client disconnected"),
+            Message::NewMessage(data) => println!("INFO: Received: {:?}", data),
+        }
+    }
+    Ok(())
 }
 
 fn client(mut stream: TcpStream, messages: Sender<Message>) -> Result<()> {
     messages.send(Message::ClientConnected).map_err(|err| {
-        eprintln!("ERROR: could not send message to the server thread: {err}");
+        eprintln!("ERROR: could not send message to the server thread: {}", err);
+        ()
     })?;
-    let mut buffer = Vec::new();
-    buffer.resize(64, 0);
+
+    let mut buffer = vec![0u8; 64];
+
     loop {
-       let n = stream.read(&mut buffer).map_err(|err| {
-        eprintln!("ERROR: error reading message from user {}", err);
-        let _ = messages.send(Message::ClientDisconnected);
-       });
-       messages.send(Message::NewMessage(buffer[0..n?])).map_err(|err| {
-        eprintln!("ERROR: could not send message to server thread: {}", err);
-       })?;
+        let n = stream.read(&mut buffer).map_err(|err| {
+            eprintln!("ERROR: error reading message from user: {}", err);
+            let _ = messages.send(Message::ClientDisconnected);
+            ()
+        })?;
+
+        if n == 0 {
+            let _ = messages.send(Message::ClientDisconnected);
+            break;
+        }
+
+        messages.send(Message::NewMessage(buffer[..n].to_vec())).map_err(|err| {
+            eprintln!("ERROR: could not send message to server thread: {}", err);
+            ()
+        })?;
     }
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
@@ -54,22 +72,31 @@ fn main() -> Result<()> {
 
     let listener = TcpListener::bind(address).map_err(|err| {
         eprintln!("ERROR: could not bind {}: {}", address, Sensitive(err));
+        ()
     })?;
 
-    println!("INFO: listening to {}", address);
-    let (message_sender, message_reciever) = channel();
-    thread::spawn(move || server(message_reciever));
+    println!("INFO: listening on {}", address);
+    let (message_sender, message_receiver) = channel();
+    thread::spawn(move || {
+        if let Err(_) = server(message_receiver) {
+            eprintln!("ERROR: server thread crashed");
+        }
+    });
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let message_sender = message_sender.clone();
-                thread::spawn(|| {|| client(stream, message_sender)});
+                let sender_clone = message_sender.clone();
+                thread::spawn(move || {
+                    if let Err(_) = client(stream, sender_clone) {
+                        eprintln!("ERROR: client thread exited with error");
+                    }
+                });
             }
             Err(err) => {
                 eprintln!("ERROR: could not accept connection: {}", err);
             }
-        };
+        }
     }
 
     Ok(())
