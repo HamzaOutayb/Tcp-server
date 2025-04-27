1d6a1e7fe0d198fc1512986b170e237d6a7b4a9b
houtaib <houtayb@gmail.com>
2025-04-27 09:15:38 +0100

    starting the server

--- main.rs
+++ main.rs
@@ -2,25 +2,38 @@ use std::net::TcpListener;
 use std::result;
 use std::io::Write;
 
-type Result<T> = result::Result<T, ()>
+type Result<T> = result::Result<T, ()>;
 
-fn main() -> Result {
+struct Sensitive<T> {
+    inner: T
+}
+
+impl<T: Display> Display for Sensitive<T>
+
+fn main() -> Result<()> {
     let address = "127.0.0.1:6969";
 
-    let listener = TcpListener::bind(address).map_err(|err|{
-        eprintln!("ERROR: could not bind {address}:{err}")
+    // Bind to the address
+    let listener = TcpListener::bind(address).map_err(|err| {
+        eprintln!("ERROR: could not bind {address}: {err}");
     })?;
-    println!("INFO: listening to {address}")
+
+    // Print a message when it's listening
+    println!("INFO: listening to {address}");
+
+    // Accept incoming connections
     for stream in listener.incoming() {
         match stream {
-            ok(mut stream) {
-                let _ = writeln!(stream, "Hello Friend").map_err(|err|{
-                    eprintln("ERROR: could not write message ro user: {err}")
-                })
+            Ok(mut stream) => {
+                let _ = writeln!(stream, "Hello Friend").map_err(|err| {
+                    eprintln!("ERROR: could not write message to user: {err}");
+                });
             }
             Err(err) => {
-                eprintln("Error: could not accept connection: {err}")
+                eprintln!("ERROR: could not accept connection: {err}");
             }
         }
     }
+
+    Ok(())
 }
