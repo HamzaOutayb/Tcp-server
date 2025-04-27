8a2cc95f0e3a137a9c9d001f93a1a9bf3f0c48ef
houtaib <houtyab@gmail.com>
2025-04-27 14:51:40 +0100

    heeeeeelp

--- main.rs
+++ main.rs
@@ -1,38 +1,75 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::result;
-use std::io::Write;
+use std::io::Read;
+use std::fmt;
+use std::thread;
+use std::sync::mpsc::{Sender, Receiver, channel};
 
 type Result<T> = result::Result<T, ()>;
 
-struct Sensitive<T> {
-    inner: T
+struct Sensitive<T>(T);
+
+const SAFE_MODE: bool = false;
+
+impl<T: fmt::Display> fmt::Display for Sensitive<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self(inner) = self;
+        if SAFE_MODE {
+            writeln!(f, "[REDACTED]")
+        } else {
+            inner.fmt(f)
+        }
+    }
 }
 
-impl<T: Display> Display for Sensitive<T>
+enum Message {
+    ClientConnected,
+    ClientDisconnected,
+    NewMessage(Vec<u8>),
+}
+
+fn server(_message: Receiver<Message>) -> Result<()> {
+       todo!("{:?}", ())
+}
+
+fn client(mut stream: TcpStream, messages: Sender<Message>) -> Result<()> {
+    messages.send(Message::ClientConnected).map_err(|err| {
+        eprintln!("ERROR: could not send message to the server thread: {err}");
+    })?;
+    let mut buffer = Vec::new();
+    buffer.resize(64, 0);
+    loop {
+       let n = stream.read(&mut buffer).map_err(|err| {
+        eprintln!("ERROR: error reading message from user {}", err);
+        let _ = messages.send(Message::ClientDisconnected);
+       });
+       messages.send(Message::NewMessage(buffer[0..n?])).map_err(|err| {
+        eprintln!("ERROR: could not send message to server thread: {}", err);
+       })?;
+    }
+}
 
 fn main() -> Result<()> {
     let address = "127.0.0.1:6969";
 
-    // Bind to the address
     let listener = TcpListener::bind(address).map_err(|err| {
-        eprintln!("ERROR: could not bind {address}: {err}");
+        eprintln!("ERROR: could not bind {}: {}", address, Sensitive(err));
     })?;
 
-    // Print a message when it's listening
-    println!("INFO: listening to {address}");
+    println!("INFO: listening to {}", address);
+    let (message_sender, message_reciever) = channel();
+    thread::spawn(move || server(message_reciever));
 
-    // Accept incoming connections
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let _ = writeln!(stream, "Hello Friend").map_err(|err| {
-                    eprintln!("ERROR: could not write message to user: {err}");
-                });
+            Ok(stream) => {
+                let message_sender = message_sender.clone();
+                thread::spawn(|| {|| client(stream, message_sender)});
             }
             Err(err) => {
-                eprintln!("ERROR: could not accept connection: {err}");
+                eprintln!("ERROR: could not accept connection: {}", err);
             }
-        }
+        };
     }
 
     Ok(())
